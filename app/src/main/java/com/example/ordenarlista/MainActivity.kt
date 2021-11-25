001ad56cd23f0058684a4b838aa0c89596ee325f
ArterialK <42286659+ArterialK@users.noreply.github.com>
2021-11-25 16:18:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ordenarlista/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.example.ordenarlista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.*
+import androidx.appcompat.widget.SwitchCompat
+
+class MainActivity : AppCompatActivity() {
+    private var lista = mutableListOf<Float>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val etNum = findViewById<EditText>(R.id.etNum)
+        etNum.setOnClickListener {
+        }
+    }
+    private fun checarNum(): Boolean{
+        val etNum = findViewById<EditText>(R.id.etNum)
+
+        if (etNum.text.isEmpty()){
+            Toast.makeText(this, getString(R.string.errorSinNum),Toast.LENGTH_LONG).show()
+            return false
+        }else if(etNum.text.contains('-')){
+            if(!etNum.text.contains('.')){
+                if(etNum.text.length == 7){
+                    etNum.text = null
+                    Toast.makeText(this, getString(R.string.errorLongitud),Toast.LENGTH_LONG).show()
+                    return false
+                }
+            }
+        } else{
+            if(etNum.text.contains('.')){
+                if(etNum.text.length > 6){
+                    etNum.text = null
+                    Toast.makeText(this, getString(R.string.errorLongitud),Toast.LENGTH_LONG).show()
+                    return false
+                }
+            } else if(etNum.text.length > 5){
+                etNum.text = null
+                Toast.makeText(this, getString(R.string.errorLongitud),Toast.LENGTH_LONG).show()
+                return false
+            }
+        }
+        lista.add(etNum.text.toString().toFloat())
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.arriba -> ascendente()
+            R.id.abajo -> descendente()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun ascendente(){
+        val adaptador: ArrayAdapter<Float>
+        val listNum = findViewById<ListView>(R.id.listNumeros)
+        if(lista.isEmpty()){
+            Toast.makeText(this, getString(R.string.errorListaVacia),Toast.LENGTH_LONG).show()
+        }else{
+            lista.sort()
+            adaptador = ArrayAdapter(this,android.R.layout.simple_list_item_1,lista)
+            listNum.adapter = adaptador
+        }
+    }
+
+    private fun descendente(){
+        val adaptador: ArrayAdapter<Float>
+        val listNum = findViewById<ListView>(R.id.listNumeros)
+        if(lista.isEmpty()){
+            Toast.makeText(this, getString(R.string.errorListaVacia),Toast.LENGTH_LONG).show()
+        }else{
+            lista.sortDescending()
+            adaptador = ArrayAdapter(this,android.R.layout.simple_list_item_1,lista)
+            listNum.adapter = adaptador
+        }
+    }
+
+    fun ingresarDato(view: View) {
+        val adaptador: ArrayAdapter<Float>
+        val etNum = findViewById<EditText>(R.id.etNum)
+        val listNum = findViewById<ListView>(R.id.listNumeros)
+        if(checarNum()){
+            etNum.text = null
+            adaptador = ArrayAdapter(this,android.R.layout.simple_list_item_1,lista)
+            listNum.adapter = adaptador
+        }
+    }
+
+    fun decimalOn(view: View) {
+        val etNum = findViewById<EditText>(R.id.etNum)
+        val swNegativo = findViewById<SwitchCompat>(R.id.swNegativo)
+        etNum.inputType = 8194
+        swNegativo.setOnClickListener {
+            etNum.inputType = 12290
+        }
+    }
+    fun negativoOn(view: View) {
+        val etNum = findViewById<EditText>(R.id.etNum)
+        val swDecimal = findViewById<SwitchCompat>(R.id.swDecimal)
+        etNum.inputType = 4098
+        swDecimal.setOnClickListener {
+            etNum.inputType = 12290
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
